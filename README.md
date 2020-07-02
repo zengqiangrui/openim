d762a240e91691ec36840f436c73e97deea2c4ef
zengqiangrui <zengqiangrui@163.com>
2020-07-02 10:39:32 +0800

    init project

---
+++ README.md
@@ -0,0 +1,20 @@
+## openim 
+
+### 模块介绍
+```
+    openim-api:对外提供接口
+    openim-app:核心业务流程
+    openim-common:通用相关的工具包
+    openim-dto:通用对象实体
+```
+
+### 构建流程
+* openim-dto->openim-api->openim-app<-openim-common
+
+### 技术栈
+```
+    Maven 3+,Java 11,Springboot 2+,MySQL8+,Redis5+,Kafka2+
+```
+
+### 核心业务功能
+* 对外提供im openApi
---
+++ openim-app/src/main/java/indi/qiangrui/openim/app/Application.java
@@ -0,0 +1,17 @@
+package indi.qiangrui.openim.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Johnny
+ * @date 2020/7/2
+ * @description springboot app runner
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
